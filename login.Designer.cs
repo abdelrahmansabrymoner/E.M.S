e7fa8b3bc74ffdac52c43959216ab60056595cba
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-24 20:04:37 +0200

    customize the login window

--- login.Designer.cs
+++ login.Designer.cs
@@ -38,7 +38,7 @@
             this.panel1.ForeColor = System.Drawing.Color.Black;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(254, 491);
+            this.panel1.Size = new System.Drawing.Size(310, 491);
             this.panel1.TabIndex = 0;
             // 
             // login
