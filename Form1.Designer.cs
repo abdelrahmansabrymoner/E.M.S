037103e623bd8b81c2cb393f937e5c00a113e3ca
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-24 21:16:18 +0200

    customize the new label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -76,9 +76,9 @@
             this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label3.Location = new System.Drawing.Point(51, 333);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 28);
+            this.label3.Size = new System.Drawing.Size(177, 28);
             this.label3.TabIndex = 7;
-            this.label3.Text = "User Name";
+            this.label3.Text = "Employee Name";
             // 
             // textBox1
             // 
