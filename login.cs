eb74df59e5576f881a33547570f877d137875cf7
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-24 20:45:18 +0200

    add text box

--- login.cs
+++ login.cs
@@ -16,5 +16,15 @@ namespace E.M.S
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
