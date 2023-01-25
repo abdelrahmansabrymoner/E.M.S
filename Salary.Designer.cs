9752335faaef0f5d0dda6e2de46f115dc942c047
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-25 11:48:14 +0200

    edit the start poition

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -40,7 +40,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Salary";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Salary";
             this.ResumeLayout(false);
 
