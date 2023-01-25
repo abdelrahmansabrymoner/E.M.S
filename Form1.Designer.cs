b05599f335568ea28d8a394493b23164fbca447b
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-25 10:03:30 +0200

    another editing in label 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -106,9 +106,9 @@
             this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label4.Location = new System.Drawing.Point(41, 410);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 28);
+            this.label4.Size = new System.Drawing.Size(189, 28);
             this.label4.TabIndex = 9;
-            this.label4.Text = "EmployeGender ";
+            this.label4.Text = "Employe Gender ";
             // 
             // textBox2
             // 
