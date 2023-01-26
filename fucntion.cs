b0a6def872194dc0ca1164a371f603d9da3c300a
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-26 19:38:52 +0200

    add string query and add message box

--- fucntion.cs
+++ fucntion.cs
@@ -35,3 +35,16 @@ namespace EmployeeMgmt1
                 else
                 {
                     string Dep = DepNameTb.Text;
+                    string Query = "insert into DepartmentTb1 values('{0}')";
+                    Query = string.Format(Query, DepNameTb.Text);
+                    Con.SetData(Query);
+                    ShowDepartments();
+                    MessageBox.Show("Department Added!!!");
+                    DepNameTb.Text = "";
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
