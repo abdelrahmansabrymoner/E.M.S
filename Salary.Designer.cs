e18de592789d9718047861ded97449fff77fb487
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-26 18:40:06 +0200

    delete first txt box

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -43,7 +43,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -239,13 +238,6 @@
             this.label3.TabIndex = 29;
             this.label3.Text = "Employee";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(89, 364);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(292, 39);
-            this.textBox1.TabIndex = 28;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -310,7 +302,6 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.panel1);
@@ -344,7 +335,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label7;
