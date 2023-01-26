ac6e928f724e3966abc965d62cbbccaa657aeabb
Abdelrahman Sabry <abodysabrys@email.com>
2023-01-26 18:58:02 +0200

    add a new labels in department window

--- Department.Designer.cs
+++ Department.Designer.cs
@@ -200,7 +200,7 @@
             this.label8.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label8.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label8.Location = new System.Drawing.Point(639, 513);
+            this.label8.Location = new System.Drawing.Point(1162, 166);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(82, 28);
             this.label8.TabIndex = 48;
